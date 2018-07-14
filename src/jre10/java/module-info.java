0154be22a7cab879afc5c1d9a34d6e73a1773d55
GedMarc <ged_marc@hotmail.com>
2018-07-14 03:43:59 +0200

    JDK 10 Structure and Configuration

---
+++ src/jre10/java/module-info.java
@@ -0,0 +1,11 @@
+module com.jwebmp.plugins.themify.icons {
+
+	exports com.jwebmp.plugins.themify.icons;
+
+	requires com.jwebmp.core;
+	requires com.jwebmp.logmaster;
+	requires com.fasterxml.jackson.annotation;
+
+	requires java.validation;
+	requires java.logging;
+}
