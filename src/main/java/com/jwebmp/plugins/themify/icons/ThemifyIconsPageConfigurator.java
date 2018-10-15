5cc8980fed4116ecca6104e3995c3fcc56113e99
GedMarc <ged_marc@hotmail.com>
2018-10-15 23:47:07 +0200

    TypeSafety across service loaders

--- src/main/java/com/jwebmp/plugins/themify/icons/ThemifyIconsPageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/themify/icons/ThemifyIconsPageConfigurator.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotNull;
  * Configures the page for themify icons
  */
 public class ThemifyIconsPageConfigurator
-		implements IPageConfigurator
+		implements IPageConfigurator<ThemifyIconsPageConfigurator>
 {
 	/**
 	 * Field reference
