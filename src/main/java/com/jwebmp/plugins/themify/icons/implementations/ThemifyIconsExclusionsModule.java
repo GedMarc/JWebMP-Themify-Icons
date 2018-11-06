32f0c6e5234e067fadad5f0c181e711fc6ed1288
GedMarc <ged_marc@hotmail.com>
2018-11-06 21:28:56 +0200

    JRE 11 full split and module specifications a bit stricter

---
+++ src/main/java/com/jwebmp/plugins/themify/icons/implementations/ThemifyIconsExclusionsModule.java
@@ -0,0 +1,30 @@
+package com.jwebmp.plugins.themify.icons.implementations;
+
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
+
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ThemifyIconsExclusionsModule
+		implements IGuiceScanModuleExclusions<ThemifyIconsExclusionsModule>,
+				           IGuiceScanJarExclusions<ThemifyIconsExclusionsModule>
+{
+
+	@Override
+	public @NotNull Set<String> excludeJars()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("jwebmp-themify-icons-*");
+		return strings;
+	}
+
+	@Override
+	public @NotNull Set<String> excludeModules()
+	{
+		Set<String> strings = new HashSet<>();
+		strings.add("com.jwebmp.plugins.themify.icons");
+		return strings;
+	}
+}
