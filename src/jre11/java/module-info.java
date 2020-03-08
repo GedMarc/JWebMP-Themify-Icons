20f1d5ef5bfb6cc738dd30d58c86e90a57e44772
GedMarc <ged_marc@hotmial.com>
2020-03-08 17:00:07 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -12,7 +12,6 @@ module com.jwebmp.plugins.themify.icons {
 
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.themify.icons.ThemifyIconsPageConfigurator;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.themify.icons.implementations.ThemifyIconsExclusionsModule;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.themify.icons.implementations.ThemifyIconsExclusionsModule;
 
 	//opens com.jwebmp.plugins.themify to com.fasterxml.jackson.databind,com.jwebmp.core;
 	opens com.jwebmp.plugins.themify.icons to com.fasterxml.jackson.databind, com.jwebmp.core;
--- src/main/java/com/jwebmp/plugins/themify/icons/implementations/ThemifyIconsExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/themify/icons/implementations/ThemifyIconsExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.themify.icons.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,18 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ThemifyIconsExclusionsModule
-		implements IGuiceScanModuleExclusions<ThemifyIconsExclusionsModule>,
-				           IGuiceScanJarExclusions<ThemifyIconsExclusionsModule>
+		implements IGuiceScanModuleExclusions<ThemifyIconsExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-themify-icons-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
